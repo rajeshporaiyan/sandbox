71ca7c37bb8a74fe774a51d64530c7c42a110451
rajeshporaiyan <rajeshporaiyan@gmail.com>
2017-01-10 21:55:29 -0500

    #1 renamed package names

--- event-store/src/main/java/io/cloud/EventStoreApplication.java
+++ event-store/src/main/java/io/cloud/EventStoreApplication.java
@@ -1,4 +1,4 @@
-package io.rajeshporaiyan;
+package io.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- event-store/src/test/java/io/cloud/EventStoreApplicationTests.java
+++ event-store/src/test/java/io/cloud/EventStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package io.rajeshporaiyan;
+package io.cloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
